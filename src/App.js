40685dd2f7b87acf5b0e4a2434dbe970ae331abe
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-11-25 18:53:38 +0000

    made the header component, fetching the api and will work on it later

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  // https://disease.sh/v3/covid-19/countries
+
+  //useEFfect it runs a piece of code based on a given conditon
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
+
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>COvid Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name} </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats"></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/InfoBox.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./InfoBox.css";
+function InfoBox() {
+  return <div></div>;
+}
+
+export default InfoBox;
