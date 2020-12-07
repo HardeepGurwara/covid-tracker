cea77540f47d6bba02993f664288b1e87af22d18
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-12-07 21:23:00 +0000

    added the app structure, now need to design it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import { FormControl, MenuItem, Select } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  FormControl,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
 import { useEffect, useState } from "react";
 import "./App.css";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -32,18 +40,37 @@ function App() {
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COvid Tracker</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country} onChange={onCountryChange}>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name} </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COvid Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">WorldWide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name} </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox title="Coronavirus cases" total={2000} cases={122} />
+          <InfoBox title="Recovered" total={3000} cases={111} />
+          <InfoBox title="Deaths" total={100} cases={5} />
+        </div>
+
+        <Map />
       </div>
-      <div className="app__stats"></div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by country</h3>
+
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,7 +1,22 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
 import React from "react";
 import "./InfoBox.css";
-function InfoBox() {
-  return <div></div>;
+function InfoBox({ title, cases, total }) {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
+
+        <h2 className="infoBox__cases">{cases}</h2>
+
+        <Typography color="textSecondary" className="infoBox__total">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default InfoBox;
